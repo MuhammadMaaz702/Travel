5a06804a2c65fb5d7839169f21e59a3c012961e5
maaz.virtuenetz@gmail.com <maaz.virtuenetz@gmail.com>
2023-05-17 16:08:47 +0500

    my commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          upload to vercel
+          upload to vercel by maaz
         </p>
         <a
           className="App-link"
